6f3c8c14e0f7730847c01cc89b1ce73f96540ba2
Pritam Chakraborty <pritam.amit29@gmail.com>
2024-08-20 04:01:56 +0200

    h.34e

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -7,7 +7,7 @@ import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
-    <header className="w-full border-b">
+    <header className="w-full border-b px-4">
       <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
           <Image
