870b94d8e8bb4189b38d51f2242b7bcc0cf9073c
Pritam Chakraborty <pritam.amit29@gmail.com>
2024-08-20 16:36:35 +0200

    h3

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -59,7 +59,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
           <CategoryFilter />
         </div>
 
-        <Collection
+        {/* <Collection
           data={events?.data}
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
@@ -67,7 +67,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
           limit={6}
           page={page}
           totalPages={events?.totalPages}
-        />
+        /> */}
       </section>
     </>
   );
